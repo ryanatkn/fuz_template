f40a3e90bfba1b5a5222e202ba42949009425104
Ryan Atkinson <2608646+ryanatkn@users.noreply.github.com>
2021-10-02 02:24:19 -0500

    upgrade deps and add CI (#2)

--- src/lib/ui/Mreows.svelte
+++ src/lib/ui/Mreows.svelte
@@ -1,13 +1,13 @@
 <script lang="ts">
 	import {icons} from '@feltcoop/felt';
-	import {random_item} from '@feltcoop/felt/util/random.js';
+	import {randomItem} from '@feltcoop/felt/util/random.js';
 
 	const items: string[] = Object.values(icons);
 
 	let mreows = ['🐱'];
 
 	const mreow = (): void => {
-		mreows = mreows.concat(random_item(items)!);
+		mreows = mreows.concat(randomItem(items)!);
 	};
 </script>
 
--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,15 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import static_adapter from '@sveltejs/adapter-static';
 
+const dev = process.env.NODE_ENV !== 'production';
+
 /** @type {import('@sveltejs/kit').Config} */
 export default {
 	preprocess: typescript(),
 	kit: {
 		adapter: static_adapter(),
 		target: '#root',
-		paths: {base: '/felt-template'}, // for GitHub pages -- delete this line for top-level domains
+		paths: dev ? undefined : {base: '/felt-template'}, // for GitHub pages -- delete this line for top-level domains
 		files: {assets: 'src/static'},
 		vite: {
 			ssr: {
