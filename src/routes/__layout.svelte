dab62a4cdd6e393b7dc2f777dea1cd9f4f794305
Ryan Atkinson <mail@ryanatkn.com>
2022-01-15 17:12:14 -0600

    add base path to favicon link

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
 	import '@feltcoop/felt/ui/style.css';
+	import {base} from '$app/paths';
 
 	import '$lib/style.css';
 </script>
 
 <svelte:head>
 	<title>felt-template</title>
-	<link rel="icon" href="/favicon.png" />
+	<link rel="icon" href="{base}/favicon.png" />
 </svelte:head>
 
 <slot />
