03dce2d6eb53207d6e02453c2eb73ad14d4ebd09
Ryan Atkinson <mail@ryanatkn.com>
2022-05-13 22:07:35 -0500

    upgrade deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,5 @@
 import {typescript} from 'svelte-preprocess-esbuild';
-import static_adapter from '@sveltejs/adapter-static';
+import adapter from '@sveltejs/adapter-static';
 
 const dev = process.env.NODE_ENV !== 'production';
 
@@ -10,7 +10,7 @@ export default {
 		immutable: true,
 	},
 	kit: {
-		adapter: static_adapter(),
+		adapter: adapter(),
 		paths: dev ? undefined : {base: '/felt-template'}, // for GitHub pages -- delete this line for top-level domains
 		files: {assets: 'src/static'},
 		prerender: {default: true},
