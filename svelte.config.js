b289aa348eebd9da41e270d9289d55b923c50837
Ryan Atkinson <mail@ryanatkn.com>
2022-11-07 11:02:09 -0600

    upgrade deps

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,7 @@
 import {typescript} from 'svelte-preprocess-esbuild';
 import adapter from '@sveltejs/adapter-static';
 
-// const dev = process.env.NODE_ENV !== 'production'; // see below
+// const dev = process.argv.includes('dev'); // see below
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
