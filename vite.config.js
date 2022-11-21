485c42cde5e28043282e70fcc3a278d1ce1ddb11
Ryan Atkinson <mail@ryanatkn.com>
2022-11-20 23:22:03 -0600

    set env prefix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
+	envPrefix: 'PUBLIC_',
 	optimizeDeps: {exclude: ['@feltcoop/felt']},
 };
 
