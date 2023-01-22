dda51064b8a1fa92795bfca7f53f782d4638dcdc
Ryan Atkinson <mail@ryanatkn.com>
2023-01-21 17:41:33 -0700

    upgrade deps

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	envPrefix: 'PUBLIC_',
 	optimizeDeps: {exclude: ['@feltcoop/felt']},
 };
 
