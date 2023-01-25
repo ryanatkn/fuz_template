f565163c1c26a788f2d348177e3ed2dac11f1a8d
Ryan Atkinson <mail@ryanatkn.com>
2023-01-25 15:23:06 -0700

    rename

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import {sveltekit} from '@sveltejs/kit/vite';
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
-	optimizeDeps: {exclude: ['@feltjs/felt']},
+	optimizeDeps: {exclude: ['@feltjs/felt-ui']},
 };
 
 export default config;
