3dabb9191c4141100e83fcea59fa6c715bb01832
Ryan Atkinson <mail@ryanatkn.com>
2023-02-01 01:07:04 -0700

    link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,13 +1,11 @@
 <script lang="ts">
 	import '@feltjs/felt-ui/style.css';
-	import {base} from '$app/paths';
 
 	import './style.css';
 </script>
 
 <svelte:head>
 	<title>felt-template</title>
-	<link rel="icon" href="{base}/favicon.png" />
 </svelte:head>
 
 <slot />
