207d861b6b66078ce6ec15d51428799e67865a0e
Ryan Atkinson <mail@ryanatkn.com>
2023-06-28 16:41:21 -0600

    cleanup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,9 +3,6 @@
 	import '@feltjs/felt-ui/theme.css';
 	import '$routes/style.css';
 
-	import {writable} from 'svelte/store';
-	import {loadTheme} from '@feltjs/felt-ui/theme.js';
-	import {DEFAULT_THEME, defaultThemes} from '@feltjs/felt-ui/themes.js';
 	import Themed from '@feltjs/felt-ui/Themed.svelte';
 	import Dialog from '@feltjs/felt-ui/Dialog.svelte';
 	import Contextmenu from '@feltjs/felt-ui/Contextmenu.svelte';
@@ -14,13 +11,6 @@
 	import ContextmenuTextEntry from '@feltjs/felt-ui/ContextmenuTextEntry.svelte';
 
 	import Settings from '$lib/Settings.svelte';
-
-	// Setup the UI theme.
-	const loadedTheme = loadTheme();
-	// TODO hacky remove this when adding runtime theme editing
-	const loadedThemeDefaultRef =
-		loadedTheme && defaultThemes.find((t) => t.name === loadedTheme.name);
-	const theme = writable(loadedThemeDefaultRef || loadedTheme || DEFAULT_THEME);
 
 	const contextmenu = createContextmenu({
 		linkComponent: ContextmenuLinkEntry,
@@ -34,7 +24,7 @@
 	<title>felt-template</title>
 </svelte:head>
 
-<Themed {theme}>
+<Themed>
 	<div
 		use:contextmenu.action={{
 			content: 'Settings',
