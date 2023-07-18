f690318a8523fd1db6408f687dc5daab158fa01c
Ryan Atkinson <mail@ryanatkn.com>
2023-07-17 20:17:33 -0600

    add an example route (#11)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,23 +1,26 @@
 <script lang="ts">
 	import Message from '@feltjs/felt-ui/Message.svelte';
+	import {base} from '$app/paths';
 
+	import Header from '$routes/Header.svelte';
 	import Mreows from '$routes/Mreows.svelte';
 
 	let mreows: Array<{icon: string}> | undefined;
 </script>
 
 <main class="prose">
-	<section>
-		<header class="box row">
-			<h1>felt<small><a href="https://github.com/feltjs">💚</a></small>template</h1>
-		</header>
-		<Message
-			><span slot="icon"
+	<section class="box">
+		<Header />
+		<Message>
+			<span slot="icon"
 				>{#if mreows}{mreows[0].icon}{:else}✨{/if}</span
 			><span
 				>hello, welcome to <a href="https://github.com/feltjs/felt-template">felt-template</a></span
-			></Message
-		>
+			>
+		</Message>
+		<p>
+			here's an <a href="{base}/route">example route</a>
+		</p>
 	</section>
 	<Mreows bind:mreows />
 </main>
@@ -28,8 +31,5 @@
 		flex-direction: column;
 		align-items: center;
 		margin: 0 auto;
-	}
-	h1 {
-		text-align: center;
 	}
 </style>
