e6b0898b2bd382ca20922abe1793794ba5f42a7a
Ryan Atkinson <mail@ryanatkn.com>
2023-09-19 07:00:34 -0600

    upgrade gro (#12)

--- src/lib/example.test.ts
+++ src/lib/example.test.ts
@@ -1,7 +1,7 @@
 import {test} from 'uvu';
 import * as assert from 'uvu/assert';
 
-import {a} from '$lib/index';
+import {a} from '$lib/example';
 
 test('everything looks good', () => {
 	assert.is(a, 5);
