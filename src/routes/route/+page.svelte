a9c71d3c708418e3d4080b779936b58046643ab4
Ryan Atkinson <mail@ryanatkn.com>
2023-10-01 11:39:01 -0600

    upgrade deps and improve website

--- src/routes/route/+page.svelte
+++ src/routes/route/+page.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
 	import Breadcrumb from '@fuz.dev/fuz_library/Breadcrumb.svelte';
+	import {parse_package_meta} from '@fuz.dev/fuz_library/package.js';
+	import LibraryHeader from '@fuz.dev/fuz_library/LibraryHeader.svelte';
+	import LibraryFooter from '@fuz.dev/fuz_library/LibraryFooter.svelte';
 
 	import Header from '$routes/Header.svelte';
+
+	// TODO SvelteKit warns about this but we put `/static` in `/src` because of what it's saying,
+	/// maybe change to import as the first item from `packages`
+	import package_json from '../../static/.well-known/package.json';
+	const pkg = parse_package_meta(package_json.homepage, package_json);
 </script>
 
-<main class="prose">
-	<section class="box">
+<main class="box width_full">
+	<section class="box prose">
 		<Header />
 		<p>this is an example route</p>
-		<Breadcrumb>🏠</Breadcrumb>
 	</section>
+	<section><Breadcrumb>🏠</Breadcrumb></section>
+	<div class="box width_sm">
+		<section>
+			<LibraryHeader {pkg} />
+		</section>
+		<section>
+			<LibraryFooter {pkg} root_url="https://www.fuz.dev/" />
+		</section>
+	</div>
 </main>
 
 <style>
@@ -18,5 +34,8 @@
 		flex-direction: column;
 		align-items: center;
 		margin: 0 auto;
+	}
+	section {
+		margin-bottom: var(--spacing_xl3);
 	}
 </style>
