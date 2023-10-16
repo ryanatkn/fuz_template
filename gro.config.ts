cf327c3d1172525f2d2c75f95d4b3db2d484e902
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 10:12:42 -0600

    move static to root (#15)

--- gro.config.ts
+++ gro.config.ts
@@ -1,11 +1,22 @@
-import type {GroConfigCreator} from '@grogarden/gro';
+import {type CreateGroConfig, replace_plugin} from '@grogarden/gro';
 
-const config: GroConfigCreator = async (cfg) => {
-	// this template's `package.json` has `"private": true`,
-	// which disables the `.well-known/package.json`,
-	// so we re-enable it here -
-	// @see https://github.com/grogarden/gro/blob/main/src/lib/docs/config.md#when-updating_well_known
-	cfg.package_json = (pkg, when) => (when === 'updating_well_known' ? pkg : null);
+// This config file can be deleted for projects that want the normal defaults. See below for more.
+// See Gro for more: https://github.com/grogarden/gro
+
+const config: CreateGroConfig = async (cfg) => {
+	// This template's `package.json` has `"private": true` to protect users,
+	// disabling `.well-known/package.json`, but in this case we want to publish it,
+	// so we re-enable it here.
+	// See the Gro docs for more about `.well-known/package.json`:
+	// https://github.com/grogarden/gro/blob/main/src/lib/docs/gro_plugin_sveltekit_frontend.md
+	const get_base_plugins = cfg.plugins;
+	cfg.plugins = async (ctx) =>
+		replace_plugin(
+			await get_base_plugins(ctx),
+			(await import('@grogarden/gro/gro_plugin_sveltekit_frontend.js')).plugin({
+				well_known_package_json: true,
+			}),
+		);
 
 	return cfg;
 };
--- svelte.config.js
+++ svelte.config.js
@@ -1,19 +1,16 @@
-//@ts-expect-error
-import {typescript} from 'svelte-preprocess-esbuild';
+import {vitePreprocess} from '@sveltejs/kit/vite';
 import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 export default {
-	preprocess: typescript(),
+	preprocess: [vitePreprocess()],
 	compilerOptions: {immutable: true},
 	vitePlugin: {inspector: true}, // docs: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/inspector.md
 	kit: {
 		adapter: adapter(),
-		// this is for non-root domains like the default GitHub Pages
-		// `username.github.io/repo` where `repo` is `base` -
-		// delete these comments for top-level domains
-		// paths: import.meta.env.DEV ? undefined : {base: '/template'},
-		files: {assets: 'src/static'},
 		alias: {$routes: 'src/routes', $fixtures: 'src/fixtures'},
+		// This `paths` config is for non-root domains like the default GitHub Pages
+		// `username.github.io/repo` where `repo` is `base` - delete this comment for top-level domains.
+		// paths: import.meta.env.DEV ? undefined : {base: '/template'},
 	},
 };
