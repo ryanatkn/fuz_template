99d72b3348e37c59097ee90f645f7dea3efeb1da
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 09:38:38 -0600

    wip

--- gro.config.ts
+++ gro.config.ts
@@ -1,17 +1,19 @@
 import {type CreateGroConfig, replace_plugin} from '@grogarden/gro';
 
+// This config file can be deleted for projects that want the normal defaults. See below for more.
+
 const config: CreateGroConfig = async (cfg) => {
 	// This template's `package.json` has `"private": true` to protect users,
 	// disabling `.well-known/package.json`, but in this case we want to publish it,
 	// so we re-enable it here.
-	// This entire config file can be deleted for projects that want the normal defaults.
-	// @see https://github.com/grogarden/gro/blob/main/src/lib/docs/config.md#when-updating_well_known
+	// See the Gro docs for more about `.well-known/package.json`:
+	// https://github.com/grogarden/gro/blob/main/src/lib/docs/gro_plugin_sveltekit_frontend.md
 	const get_base_plugins = cfg.plugins;
 	cfg.plugins = async (ctx) =>
 		replace_plugin(
 			await get_base_plugins(ctx),
 			(await import('@grogarden/gro/gro_plugin_sveltekit_frontend.js')).plugin({
-				well_known_package_json: true, // TODO BLOCK test mapping and false and undefined
+				well_known_package_json: true,
 			}),
 		);
 
