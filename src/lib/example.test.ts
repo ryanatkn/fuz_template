9d0670d1e488bb90581ea991d65fc5ba2ca28311
Ryan Atkinson <mail@ryanatkn.com>
2023-10-16 08:48:33 -0600

    wip

--- src/lib/example.test.ts
+++ src/lib/example.test.ts
@@ -1,5 +1,7 @@
 /**
+ * usage: `npm test` or `gro test`
  * @see https://github.com/lukeed/uvu
+ * @see https://github.com/grogarden/gro/blob/main/src/lib/docs/test.md
  */
 
 import {test} from 'uvu';
--- svelte.config.js
+++ svelte.config.js
@@ -8,10 +8,9 @@ export default {
 	vitePlugin: {inspector: true}, // docs: https://github.com/sveltejs/vite-plugin-svelte/blob/main/docs/inspector.md
 	kit: {
 		adapter: adapter(),
-		// this is for non-root domains like the default GitHub Pages
-		// `username.github.io/repo` where `repo` is `base` -
-		// delete these comments for top-level domains
-		// paths: import.meta.env.DEV ? undefined : {base: '/template'},
 		alias: {$routes: 'src/routes', $fixtures: 'src/fixtures'},
+		// This `paths` config is for non-root domains like the default GitHub Pages
+		// `username.github.io/repo` where `repo` is `base` - delete this comment for top-level domains.
+		// paths: import.meta.env.DEV ? undefined : {base: '/template'},
 	},
 };
