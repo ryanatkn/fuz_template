0d1cbf722d1656944a2659d3d56d1275f0ca3a7d
Ryan Atkinson <mail@ryanatkn.com>
2023-10-24 16:54:41 -0600

    wip

--- src/lib/example.ts
+++ src/lib/example.ts
@@ -1,5 +1,5 @@
 /**
- * this gets published to npm using @sveltejs/package
+ * This package can be published to npm with `gro publish` you run `npm i -D @sveltejs/package`.
  * @see https://kit.svelte.dev/docs/packaging
  */
 export const a = 5;
