4d05149bc5d488870ddab880c01013a416259462
Ryan Atkinson <mail@ryanatkn.com>
2023-11-26 14:36:00 -0700

    upgrade deps

--- src/routes/package.gen.ts
+++ src/routes/package.gen.ts
@@ -1,3 +1,3 @@
 // this is an optional pattern that makes your `package.json`
-// available as `$lib/package.ts` with automatic `const` types
+// available as `$routes/package.ts` with automatic `const` types
 export * from '@grogarden/gro/package.gen.js';
